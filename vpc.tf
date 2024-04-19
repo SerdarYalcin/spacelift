3468cfbe5872ccc4e6f945bafb0a456f39deba96
Serdar Yalcin <serdar.yalcin@ngpems.com.mt>
2024-04-19 12:18:20 +0200

    PolicyTest

--- vpc.tf
+++ vpc.tf
@@ -4,7 +4,7 @@
 
 resource "aws_vpc" "example_vpc" {
   #cidr_block = var.cidr_block
-  cidr_block = "192.168.100.0/24"
+  cidr_block = "192.168.101.0/24"
 
   enable_dns_support   = true   
   enable_dns_hostnames = true  
