cff34927fa4a9f3f460d205788e68c4f2a9a9d77
Serdar Yalcin <serdar.yalcin@ngpems.com.mt>
2024-04-22 10:02:45 +0200

    test

--- vpc.tf
+++ vpc.tf
@@ -3,15 +3,17 @@
 # }
 
 resource "aws_vpc" "example_vpc" {
-  cidr_block = var.cidr_block
-  #cidr_block = "192.168.101.0/24"
+  #cidr_block = var.cidr_block
+  cidr_block = "192.168.101.0/24"
 
   enable_dns_support   = true   
   enable_dns_hostnames = true  
 
   tags = {
-    #Name = "NGP-VPC3"
-    Name = var.vpc_name
+    Name = "NGP-VPC3"
+    #Name = var.vpc_name
     env  = "prod"  # Proper tagging as per the policy requirements
   }
 }
+
+# test
